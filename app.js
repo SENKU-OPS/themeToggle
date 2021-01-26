c34cba4e525d3415875eba115cf895d41a9e747c
SENKU-OPS <72332746+SENKU-OPS@users.noreply.github.com>
2021-01-26 20:34:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const toggleButton = document.querySelector('.toggle-theme-button');
+const body = document.querySelector('body');
+const toggleBall = document.querySelector('.toggle-theme-button__ball');
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    toggleBall.classList.toggle('toggle-theme-ball-style');
+    toggleButton.style.borderColor = "var(--toggleAccent)";
+
+});
